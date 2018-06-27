11d2f65ef6ca1edfd3a53ebee8b9e88c7078f0b8
ami <example@example.com>
2018-06-27 16:37:23 -0700

    lay out search page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 function Search(){
-
+  const searchGrid = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 2fr'
+  }
+  const map = {
+    background: 'url(https://briffly.com/wp-content/uploads/Google-Maps.jpg)',
+    width: '66vw',
+    height: '90vh'
+  }
   return(
     <div>
+      //filters and stuff go here
+      <div style={searchGrid}>
+        <div>
+          //Restaurant info goes here
+        </div>
+        <div style={map}>
+        </div>
+      </div>
     </div>
   );
 }
